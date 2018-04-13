fb32873c844e71fbd06e6a4bea844459b2bf6dc7
Yvette <yvettetran@gmail.com>
2018-04-12 18:01:08 -0700

    started code to scrape craigslist

--- scripts/craigslist-scrape.js
+++ scripts/craigslist-scrape.js
@@ -2,5 +2,12 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const craigslistScrape = () => {
-	return axios.get("https://sfbay.craigslist.org/search/sof?query=")
+	return axios.get('https://sfbay.craigslist.org/search/sof?query=web+developer&search_distance=50&postal=95112&employment_type=1&employment_type=3').then(res => {
+		const $ = cheerio.load(res.data);
+		let articles = [];
+
+		$('.result-info').each((i, element) => {
+			
+		});
+	});
 }
